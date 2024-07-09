6ea38ff14c684e56f2d6bcda2b798cca1fe1bc62
sandypockets <41454557+sandypockets@users.noreply.github.com>
2024-07-09 01:46:17 -0400

    Display word after 5 incorrect guesses

--- components/WordGrid/WordGrid.jsx
+++ components/WordGrid/WordGrid.jsx
@@ -5,8 +5,9 @@ import RemainingEmptyRows from "@/components/WordGrid/RemainingEmptyRows";
 export default function WordGrid({ challengeWord, guesses, currentGuess }) {
   const gridWidth = challengeWord.length;
   const totalGuessesAllowed = 5;
-  const emptyRows =
-    totalGuessesAllowed - guesses.length - (currentGuess ? 1 : 0);
+  const emptyRows = totalGuessesAllowed - guesses.length - (currentGuess ? 1 : 0);
+  const allGuessesUsed = guesses.length === totalGuessesAllowed;
+  const isCorrectGuessIncluded = guesses.includes(challengeWord);
 
   return (
     <div className="flex flex-col mb-4">
@@ -19,10 +20,15 @@ export default function WordGrid({ challengeWord, guesses, currentGuess }) {
           />
         </div>
       ))}
-      {currentGuess && (
+      {currentGuess && !allGuessesUsed && (
         <CurrentGuessRow gridWidth={gridWidth} currentGuess={currentGuess} />
       )}
       <RemainingEmptyRows emptyRows={emptyRows} gridWidth={gridWidth} />
+      {allGuessesUsed && !isCorrectGuessIncluded && (
+        <div className="text-center mt-4">
+          <p className="text-lg font-semibold">The correct word was: <a target="_blank" className="text-green-500 hover:text-green-600" href={`https://www.merriam-webster.com/dictionary/${challengeWord}`}>{challengeWord}</a></p>
+        </div>
+      )}
     </div>
   );
 }
