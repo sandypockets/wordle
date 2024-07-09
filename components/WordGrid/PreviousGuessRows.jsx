07c471a87db237ab2d36cb39654725e28810b62b
sandypockets <41454557+sandypockets@users.noreply.github.com>
2024-07-08 23:32:11 -0400

    Refactor, formatting

--- components/WordGrid/PreviousGuessRows.jsx
+++ components/WordGrid/PreviousGuessRows.jsx
@@ -1,23 +1,35 @@
 import { useState, useEffect } from "react";
 
-export default function PreviousGuessRows({ guess, getLetterStatusClass, challengeWord, isLastRow }) {
+export default function PreviousGuessRows({
+  guess,
+  getLetterStatusClass,
+  challengeWord,
+  isLastRow,
+}) {
   const [triggerCardFlip, setTriggerCardFlip] = useState(false);
 
   useEffect(() => {
     if (isLastRow) {
-      setTimeout(() => setTriggerCardFlip(true), 100); // Trigger flip after a short delay
+      setTimeout(() => setTriggerCardFlip(true), 300);
     }
   }, [isLastRow]);
 
   return (
     <div className="flex justify-center mb-2">
       {guess.split("").map((letter, index) => (
-        <div className={`relative h-14 w-14 mx-1 ${triggerCardFlip ? 'flip' : ''}`} key={index}>
+        <div
+          className={`relative h-14 w-14 mx-1 ${triggerCardFlip ? "flip" : ""}`}
+          key={index}
+        >
           <div className="flip-inner">
-            <div className={`flip-front absolute inset-0 border border-gray-500 flex items-center justify-center`}>
+            <div
+              className={`flip-front absolute inset-0 border border-gray-500 flex items-center justify-center`}
+            >
               {letter}
             </div>
-            <div className={`flip-back absolute inset-0 border border-gray-500 flex items-center justify-center ${getLetterStatusClass(letter, index, challengeWord)}`}>
+            <div
+              className={`flip-back absolute inset-0 border border-gray-500 flex items-center justify-center ${getLetterStatusClass(letter, index, challengeWord)}`}
+            >
               {letter}
             </div>
           </div>
